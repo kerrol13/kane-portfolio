bad1ceea82490f67d44d62a01e570523dca91894
Kane <kerrolbusiness@gmail.com>
2024-06-12 17:34:46 +0800

    change button rotation

--- src/components/icons/ArrowIcon.jsx
+++ src/components/icons/ArrowIcon.jsx
@@ -3,7 +3,7 @@ const ArrowIcon = ({color,rotate}) => {
     <svg
       viewBox="0 0 24 24"
       fill="none"
-      className={`w-[clamp(1.5rem,2vw,1.1vw)] -mt-[2px] rotate-${rotate}`}
+      className={`w-[clamp(1.5rem,2vw,1.1vw)] -mt-[2px] ${rotate?"rotate-180":"rotate-0"} `}
       >
       <path
         d="M6 12H18M6 12L11 7M6 12L11 17"
